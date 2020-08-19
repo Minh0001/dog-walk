413a237c3b46549bdcaf98faa9f384ea5a8df8a1
Minh Quang Le <quangminh0001@gmail.com>
2020-08-19 14:31:42 +0800

    Integrate react-native-background-timer

--- containers/Home.js
+++ containers/Home.js
@@ -1,13 +1,55 @@
 import React from "react";
-import { Button, StyleSheet, View, Text } from "react-native";
+import { AppState, Button, StyleSheet, View, Text } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
-import { startWalk } from "../store/actions";
+import BackgroundTimer from "react-native-background-timer";
+import NetInfo from "@react-native-community/netinfo";
+import { startWalk, clearActionQueue } from "../store/actions";
 
 export default function Home() {
+  const appState = React.useRef(AppState.currentState);
+
   const dispatch = useDispatch();
   const { actionQueue } = useSelector(({ network }) => network);
-  console.log(actionQueue);
   const onStartPress = () => dispatch(startWalk());
+
+  const handleAppStateChange = React.useCallback(
+    (nextAppState) => {
+      if (
+        appState.current.match(/inactive|active/) &&
+        nextAppState === "background"
+      ) {        
+        if (actionQueue.length) {          
+          BackgroundTimer.runBackgroundTimer(() => {
+            NetInfo.fetch().then((state) => {              
+              if (state.isConnected) {                
+                actionQueue.forEach((action) => dispatch(action));
+                dispatch(clearActionQueue());
+                BackgroundTimer.stopBackgroundTimer();
+              }
+            });
+          }, 3000);
+        }
+      }
+
+      if (
+        appState.current.match(/inactive|background/) &&
+        nextAppState === "active"
+      ) {
+        BackgroundTimer.stopBackgroundTimer();
+      }
+
+      appState.current = nextAppState;
+    },
+    [actionQueue]
+  );
+
+  React.useEffect(() => {
+    AppState.addEventListener("change", handleAppStateChange);
+
+    return () => {
+      AppState.removeEventListener("change", handleAppStateChange);
+    };
+  }, [handleAppStateChange]);
 
   return (
     <View style={styles.container}>
--- store/actions/walk.js
+++ store/actions/walk.js
@@ -1,12 +1,14 @@
 export const START_WALK_ACTION_TYPE = "START_WALK";
 export const START_WALK_SUCCESS_ACTION_TYPE = "START_WALK_SUCCESS";
 export const START_WALK_FAILED_ACTION_TYPE = "START_WALK_FAILED";
+export const CLEAR_ACTION_QUEUE_ACTION_TYPE = "CLEAR_ACTION_QUEUE";
 
 export function startWalk() {
   return {
     type: START_WALK_ACTION_TYPE,
     meta: {
       retry: true,
+      dismiss: CLEAR_ACTION_QUEUE_ACTION_TYPE,
     },
   };
 }
@@ -22,4 +24,8 @@ export function startWalkFailed(error) {
     type: START_WALK_FAILED_ACTION_TYPE,
     error,
   };
+}
+
+export function clearActionQueue() {
+  return { type: CLEAR_ACTION_QUEUE_ACTION_TYPE };
 }
